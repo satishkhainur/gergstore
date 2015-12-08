c2cff7ca39969ba312f075868090dc57fe0cd04e
skhainur <skhainur@appsriv.com>
2015-12-08 13:40:48 +0530

    integration of G+ and FB

--- GERGStore/app/src/main/java/com/example/appsriv01/gergstore/MainActivity.java
+++ GERGStore/app/src/main/java/com/example/appsriv01/gergstore/MainActivity.java
@@ -17,12 +17,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
@@ -38,7 +41,12 @@ import com.google.android.gms.common.api.Status;
 import com.google.android.gms.plus.Plus;
 import com.facebook.FacebookSdk;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends Activity implements  ConnectionCallbacks, OnConnectionFailedListener
 {
@@ -77,8 +85,9 @@ public class MainActivity extends Activity implements  ConnectionCallbacks, OnCo
 
     private ConnectionResult mConnectionResult;
 
-    private LoginButton loginFb;
-    private static String APP_ID = "1210956735597793";
+    private ImageView fbbutton;
+
+    public  static CallbackManager callbackmanager;
 
 
     @Override
@@ -86,42 +95,52 @@ public class MainActivity extends Activity implements  ConnectionCallbacks, OnCo
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        CallbackManager callbackManager =null;
+
 
         Button btnsignup = (Button) findViewById(R.id.btnsignup);
         Button btnlogin = (Button) findViewById(R.id.btnlogin);
         TextView forgotpassword = (TextView) findViewById(R.id.forgot);
-        final EditText username = (EditText) findViewById(R.id.username);
-        final EditText password = (EditText) findViewById(R.id.password);
-        sign_in_button  = (SignInButton) findViewById(R.id.sign_in_button);
+        final EditText editusername = (EditText) findViewById(R.id.username);
+        final EditText editpassword = (EditText) findViewById(R.id.password);
+        sign_in_button = (SignInButton) findViewById(R.id.sign_in_button);
 
-        loginFb= (LoginButton)findViewById(R.id.login_button);
-        loginFb.setReadPermissions("user_friends");
-        callbackManager = CallbackManager.Factory.create();
-
-        loginFb.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        fbbutton = (ImageView) findViewById(R.id.login_button);
+       /* loginFb.setReadPermissions("user_friends");
+        final CallbackManager  callbackManager = CallbackManager.Factory.create();
+        loginFb.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(LoginResult loginResult) {
-                Toast.makeText(getApplicationContext(), "In Fb success", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
 
-            }
+                loginFb.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(getApplicationContext(), "In Fb success", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, SignUp.class);
+                        startActivity(intent);
 
-            @Override
-            public void onCancel() {
-                // App code
-            }
+                    }
 
-            @Override
-            public void onError(FacebookException exception) {
-                // App code
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
             }
         });
+*/
 
-
-
-
-
-
+        fbbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onFblogin();
+            }
+        });
 
 
         //sign_in_button.setOnClickListener(this);
@@ -132,15 +151,14 @@ public class MainActivity extends Activity implements  ConnectionCallbacks, OnCo
                 .addScope(Plus.SCOPE_PLUS_LOGIN).build();
 
 
-
         sign_in_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                        signInWithGplus();
+                signInWithGplus();
 
 
-                }
+            }
 
 
         });
@@ -149,25 +167,15 @@ public class MainActivity extends Activity implements  ConnectionCallbacks, OnCo
 
 
 
-
-        /*loginFb.setOnClickListener(new View.OnClickListener() {
+        btnsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+
             }
-        });*/
-
-
-                btnsignup.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        Intent intent = new Intent(getApplicationContext(), SignUp.class);
-                        startActivity(intent);
-
-                    }
-                });
-
+        });
 
 
         btnlogin.setOnClickListener(new View.OnClickListener() {
@@ -175,18 +183,47 @@ public class MainActivity extends Activity implements  ConnectionCallbacks, OnCo
             public void onClick(View v) {
 
 
-                if (username.getText().toString().equalsIgnoreCase("admin") &&
-                        password.getText().toString().equalsIgnoreCase("admin")) {
+                final String username = editusername.getText().toString();
+                if (!isValidEmail(username)) {
+                    editusername.setError("Invalid Email");
+                }
+
+                final String password = editpassword.getText().toString();
+                if (!isValidPassword(password)) {
+                    editpassword.setError("Invalid Password");
+                }
+
+
+/*
+
+                if (editusername.getText().toString().equalsIgnoreCase("admin") &&
+                        editpassword.getText().toString().equalsIgnoreCase("admin")) {
                     Toast.makeText(getApplicationContext(), "Success ", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(getApplicationContext(), "Not Valid User", Toast.LENGTH_SHORT).show();
                 }
-
+*/
 
             }
+
+            private boolean isValidEmail(String username) {
+                String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+                Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+                Matcher matcher = pattern.matcher(username);
+                return matcher.matches();
+            }
+
+            // validating password with retype password
+            private boolean isValidPassword(String password) {
+                if (password != null && password.length() > 6) {
+                    return true;
+                }
+                return false;
+            }
+
         });
-
-
 
 
         forgotpassword.setOnClickListener(new View.OnClickListener() {
@@ -195,10 +232,75 @@ public class MainActivity extends Activity implements  ConnectionCallbacks, OnCo
                 Toast.makeText(getApplicationContext(),"You forgotten Password", Toast.LENGTH_SHORT).show();
             }
         });
-
-
-
     }
+
+
+
+
+
+
+    private void onFblogin()
+    {
+        callbackmanager = CallbackManager.Factory.create();
+
+        // Set permissions
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email", "user_photos", "public_profile"));
+
+        LoginManager.getInstance().registerCallback(callbackmanager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+                        System.out.println("Success");
+                        Intent i = new Intent(MainActivity.this,SignUp.class);
+                        startActivity(i);
+                        GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(JSONObject json, GraphResponse response) {
+                                        if (response.getError() != null) {
+                                            // handle error
+                                            System.out.println("ERROR");
+                                        } else {
+                                            System.out.println("Success");
+                                            try {
+
+                                                String jsonresult = String.valueOf(json);
+                                                System.out.println("JSON Result" + jsonresult);
+
+                                                String str_email = json.getString("email");
+                                                String str_id = json.getString("id");
+                                                String str_firstname = json.getString("first_name");
+                                                String str_lastname = json.getString("last_name");
+
+                                            } catch (JSONException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+
+                                }).executeAsync();
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        //Log.d(TAG_CANCEL,"On cancel");
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                        //  Log.d(TAG_ERROR,error.toString());
+                    }
+                });
+    }
+
+
+
+
+
+
+
 
 
 
@@ -264,6 +366,10 @@ public class MainActivity extends Activity implements  ConnectionCallbacks, OnCo
             }
         }
 
+        else {
+            callbackmanager.onActivityResult(requestCode, responseCode, intent);
+        }
+
     }
 
     @Override
@@ -303,8 +409,6 @@ public class MainActivity extends Activity implements  ConnectionCallbacks, OnCo
                 break;
         }
     }*/
-
-
 
 
 
